beb3d402025a3120fcc4bdfe94ffc0c4235418ba
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-24 21:35:02 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,2 @@
-ENV['SINATRA_ENV'] ||= "development"
-
-require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
-
-require './app'
+require './config/environment'
+require 'sinatra/activerecord/rake'
